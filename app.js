747d28f25558515428b7294320fecdfbc611db9d
Syed Amaan <75304633+Ak-407@users.noreply.github.com>
2023-02-04 16:06:48 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,78 @@
+const bodyParser = require("body-parser");
+const express = require("express");
+const { default: mongoose } = require("mongoose");
+const app = express();
+
+mongoose.set('strictQuery', true);
+// mongoose.connect("mongodb://localhost:27017/ContactAmaan", {useNewUrlParser:true} );
+mongoose.connect("mongodb+srv://jsamaan:amaan123@cluster0.vz55wc0.mongodb.net/jsamaan?retryWrites=true&w=majority",{ useNewUrlParser: true , useUnifiedTopology: true });
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+
+
+const Contacts = mongoose.Schema({
+  name: String,
+  email: String,
+  messege: String,
+});  
+
+const contact = mongoose.model("contact", Contacts);
+
+  
+
+app.get("/", function(req,res){
+    res.sendFile(__dirname+"/index.html");    
+});
+
+app.post("/", function(req,res){
+    const name = req.body.name;
+    const email = req.body.email;
+    const messege = req.body.messege;
+    console.log(name);
+    console.log(email);
+    console.log(messege);
+     
+    const contact1 = new contact({
+        name:name,
+        email:email,
+        messege:messege
+    });
+    if(name.length===0 || email.length===0){
+        res.sendFile(__dirname+"/index2.html");   
+    }else{
+    contact1.save(function(err){
+        if(!err){
+            console.log("Your work is saved.");
+        }
+    })}
+})
+app.get("/", function(req,res){
+    res.sendFile(__dirname+"/index2.html");    
+});
+app.post("/", function(req,res){
+    const name = req.body.name;
+    const email = req.body.email;
+    const messege = req.body.messege;
+    console.log(name);
+    console.log(email);
+    console.log(messege);
+     
+    const contact1 = new contact({
+        name:name,
+        email:email,
+        messege:messege
+    });
+    if(name.length===0 || email.length===0){
+        res.sendFile(__dirname+"/index2.html");   
+    }else{
+    contact1.save(function(err){
+        if(!err){
+            console.log("Your work is saved.");
+        }
+    })}
+})
+
+app.listen("3000", function(req,res){
+    console.log("port is under 3000");
+})
